687fee40d2e4e33926262f3fdeb72d9f6a28f60d
arvind pamuri <aravind.pamuri@gmail.com>
2020-06-22 08:48:49 +0530

    SPA Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './Homecomponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './Footercomponent';
 import { DISHES } from '../shared/dishes';
@@ -33,13 +34,22 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+
+    }
+
     return (
       <div>
         <Header/>
         <Switch>
           <Route path='/home' component={HomePage} />
-          <Route path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
-          <Route path='/contactus' component={Contact} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
         <Footer/>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
 function RenderMenuItem({dish, onClick}) {
     return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-        <Card onClick={() => onClick(dish.id)}>
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
+            </Link>
         </Card>
-        </div>
     );
 }
 
@@ -20,15 +21,29 @@ const Menu = (props) => {
 
     const menu = props.dishes.map((dish) => {
         return (
-            <RenderMenuItem dish={dish} onClick={props.onClick} />
+            <div className='col-12 col-md-5 m-1'>
+                <RenderMenuItem dish={dish}/>
+            </div>
         );
         
     });
 
     return (
+        <div className='container'>
+            <div className='row'>
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+            </div>
             <div className="row">
                 {menu}
             </div>
+        </div>
     );
 }
 
