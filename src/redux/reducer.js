6020f4e4fa42b45c1531bb0984efe66d48c49ed5
arvind pamuri <aravind.pamuri@gmail.com>
2020-07-08 11:26:39 +0530

    handling fetch errors

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,7 +2,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-
+ 
 export const initialState = { 
     dishes: DISHES,
     comments: COMMENTS,
